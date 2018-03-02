e385096b847df6a42feb439b18c5caaf9f916947
GattyGamaun <gattygamaun@gmail.com>
2018-03-02 17:59:19 +0300

    zeros/fix: wrong solution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 module.exports = function getZerosCount(number) {
 
   let a = 5, b = 5, c = 0;
-  let int =(number / 5);
+  let int = Math.floor(number / 5);
   let remain = 0;
 
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < 10; i++) {
     c = a * b;
     a = c;
 
-    remain += (number / c);
+    remain += Math.floor(number / c);
     console.log(remain);
   }
 
