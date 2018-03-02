fa876450dda7f45c4704bdb6e87c1e7cc922f923
GattyGamaun <gattygamaun@gmail.com>
2018-03-02 17:49:43 +0300

    zeros/feature: the first solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  // let number = 71619670;
+  let a = 5, b = 5, c = 0;
+  let int = Math.floor(number / 5);
+  let remain = 0;
+
+  for (let i = 0; i < 10; i++) {
+    c = a * b;
+    a = c;
+
+    remain += Math.floor(number / c);
+    console.log(remain);
+  }
+
+  let zeros = Math.round(int + remain);
+  return zeros;
 }
+  //console.log(divideOnFive);
+  //console.log(remain);
+  // console.log(zeros);
+
+
