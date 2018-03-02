81c05d9c7500c2418260e43d4f2219b94ec8273b
GattyGamaun <gattygamaun@gmail.com>
2018-03-02 17:53:44 +0300

    zeros/feature: dell comments

--- src/index.js
+++ src/index.js
@@ -1,22 +1,19 @@
 module.exports = function getZerosCount(number) {
-  // let number = 71619670;
+
   let a = 5, b = 5, c = 0;
-  let int = Math.floor(number / 5);
+  let int =(number / 5);
   let remain = 0;
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 20; i++) {
     c = a * b;
     a = c;
 
-    remain += Math.floor(number / c);
+    remain += (number / c);
     console.log(remain);
   }
 
   let zeros = Math.round(int + remain);
   return zeros;
 }
-  //console.log(divideOnFive);
-  //console.log(remain);
-  // console.log(zeros);
 
 
